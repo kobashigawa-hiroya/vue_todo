a6508826eae7a78322d20a16fae5c62d712b4a3d
hiroya <hiroya19890721@gmail.com>
2021-03-04 17:43:15 +0900

    cdn vue読み込み

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'HEllo world vue.js'
+    todolists: [{
+        id: 1,
+        content: "歯磨き",
+        isCompleted: false
+      },
+      {
+        id: 1,
+        content: "買い物",
+        isCompleted: false
+      },
+      {
+        id: 1,
+        content: "勉強",
+        isCompleted: false
+      },
+    ]
   }
 });
